15b8558ed038d8093be3887a9c3cc5c9ec8fc4b9
godliness <1229727237@qq.com>
2020-11-21 11:41:33 +0800

    删除测试代码

--- app/src/main/java/com/longrise/android/webview/demo/MainActivity.java
+++ app/src/main/java/com/longrise/android/webview/demo/MainActivity.java
@@ -2,11 +2,8 @@ package com.longrise.android.webview.demo;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,8 +11,6 @@ import android.view.View;
 import com.longrise.android.webview.demo.webdemo.WebDemoActivity;
 import com.longrise.android.webview.demo.webdemo.WebDemoFragment;
 import com.longrise.android.webview.demo.x5demo.WebX5DemoActivity;
-
-import java.io.File;
 
 /**
  * Created by godliness on 2020/9/1.
@@ -25,16 +20,6 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 101){
-            if(resultCode == Activity.RESULT_OK){
-                Log.e(TAG, "uri: " + data.getData());
-            }
-        }
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,21 +32,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final Intent intent = new Intent(MainActivity.this, WebDemoActivity.class);
                 startActivity(intent);
-
-//                final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//                intent.addCategory(Intent.CATEGORY_OPENABLE);
-//                intent.setType("video/*");
-//                startActivity(intent);
-
-//                Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
-//                startActivityForResult(intent, 101);
-
-//                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-////                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); //添加这一句表示对目标应用临时授权该Uri所代表的文件
-//                File tempFile = new File(getExternalFilesDir("video"),  System.currentTimeMillis()+".mp4");
-//                Uri fileVUri = FileProvider.getUriForFile(MainActivity.this, getPackageName() + ".fileprovider", tempFile);
-//                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileVUri);
-//                startActivityForResult(intent,101);
             }
         });
 
