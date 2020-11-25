69e7fea1aba4a2c81725c24ee6daa8abadc9304b
godliness <1229727237@qq.com>
2020-11-25 14:38:55 +0800

    完善测试示例

--- app/src/main/java/com/longrise/android/webview/demo/MainActivity.java
+++ app/src/main/java/com/longrise/android/webview/demo/MainActivity.java
@@ -1,11 +1,9 @@
 package com.longrise.android.webview.demo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 import com.longrise.android.webview.demo.webdemo.WebDemoActivity;
