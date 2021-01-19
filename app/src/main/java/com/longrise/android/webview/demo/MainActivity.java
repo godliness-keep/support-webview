8f0f8cc31b83cd5e2cfa74fa49375870c08c8df5
godliness <1229727237@qq.com>
2021-01-19 16:31:53 +0800

    测试最新 JS-SDK(JavaScript部分)

--- app/src/main/java/com/longrise/android/webview/demo/MainActivity.java
+++ app/src/main/java/com/longrise/android/webview/demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
 
 import com.longrise.android.webview.demo.webdemo.WebDemoActivity;
 import com.longrise.android.webview.demo.webdemo.WebDemoFragment;
